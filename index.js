8e6ea0968bb1de974c505379324d462958bbbe2e
Mihail Atanassov <atanassov.mihail@gmail.com>
2018-03-24 15:08:53 +0100

    implement GET endpoint for classifications list and classifications by id

--- index.js
+++ index.js
@@ -12,6 +12,7 @@ app.use(bodyParser.urlencoded({
 }));
 app.use(bodyParser.json());
 
+// define json schema
 var postSchema = {
   "id": "/ClassificationPost",
   "type": "object",
@@ -41,25 +42,53 @@ v.addSchema(postSchema, '/ClassificationPost');
 
 app.post('/classifications', function(request, response) {
 
+  var status;
+  // check is request body matches the ClassificationPost schema
   if (v.validate(request.body, postSchema).errors.length > 0) {
-    response.sendStatus(400)
+    response.status(400).end();
+    return;
   }
-
+  // create post request
   var classificationRequest = {
     id: uuid(),
     createdOn: Date.now(),
     data: request.body.data,
     topics: request.body.topics,
   };
-
+  // save post request in project dir ./data (db)
   fs.writeFile(__dirname + "/data/" + classificationRequest.id, JSON.stringify(classificationRequest), 'utf8', function(err) {
     if (err) {
-      console.log(err);
-      response.sendStatus(500)
+      response.status(500).end();
     }
-    response.sendStatus(201)
   });
 
+  response.json({
+    id: classificationRequest.id
+  });
+  response.status(201).end();
+});
+
+app.get('/classifications', function(request, response) {
+  var classificationsIds = fs.readdirSync(__dirname + "/data/");
+  var classificationsResponse = [];
+  classificationsIds.forEach(id => {
+    classificationsResponse.push({
+      'id': id
+    });
+  });
+  response.json(classificationsResponse);
+  response.status(200).end();
+});
+
+app.get('/classifications/:id', function(request, response) {
+  var id = request.params.id;
+  if (!fs.existsSync(__dirname + "/data/" + id)) {
+    response.status(400).end();
+    return;
+  }
+  var classificationResponse = fs.readFileSync(__dirname + "/data/" + id, 'utf8');
+  response.json(JSON.parse(classificationResponse));
+  response.status(200).end();
 });
 
 app.listen(3000, function() {
